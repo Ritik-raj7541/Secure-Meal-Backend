a6ee0121014a09b92f2d5e3bf241e8cf06162dd9
Ritik <ritikrajbhagat1@gmail.com>
2023-11-25 23:24:23 +0530

    get my qr code done

--- controllers/student/studentOps.js
+++ controllers/student/studentOps.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const QRCs = require("../../models/qrCodes");
-const {dateFormatter} = require('../../middleWare/dateFormatter') ;
-const {getSomeValue} = require('../../config/globalVariables') ;
+const { dateFormatter } = require("../../middleWare/dateFormatter");
+const { getSomeValue } = require("../../config/globalVariables");
 
 //0. check port
 // GET - api/operation/student/get-details
@@ -15,21 +15,27 @@ const getQR = asyncHandler(async (req, res) => {
   const userEmail = req.params.email;
   const dt = new Date();
   dt.setHours(0, 0, 0, 0);
-  const date = dateFormatter(dt);
+  const dateNow = await dateFormatter(dt);
 
-  const user = await QRCs.findOne({ email: userEmail});
-  const len = user.studentQr.length ;
-  if(user && len === 0)
-      res.status(200).json({message: "token not assigned yet"}) ;
-  
-  const {qrCode1, qrCode2, qrCode3, qrCode4} = user.studentQr[len-1] ;
-//   console.log(qrCode1);
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  console.log(hours, minutes, seconds);
+  const user = await QRCs.findOne({ email: userEmail });
+  const len = user.studentQr.length;
+  if (user && len === 0)
+    res.status(200).json({ message: "token not assigned yet" });
 
-  res.status(200).json({message: "all right"}) ;
+  const { date, qrCode1, qrCode2, qrCode3, qrCode4 } = user.studentQr[len - 1];
+  if (date == dateNow) {
+    const result = {
+      date,
+      qrCode1,
+      qrCode2,
+      qrCode3,
+      qrCode4,
+    };
+    res.status(200).json(result);
+  }
+  else{
+    res.status(200).json({ message: "not generated" });
+  }
 });
 
 module.exports = { check, getQR };
